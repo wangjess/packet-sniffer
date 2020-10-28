0d5bf4f9f09e3480984c3775b163f5cb810ea07b
Jessica Wang <wangjessica02@gmail.com>
2020-10-27 22:18:23 -0400

    captured wifi

--- pcap.py
+++ pcap.py
@@ -2,13 +2,24 @@ from scapy.all import *
 import os
 
 def main():
-    print("hello")
+    # print("hello")
     
-    print os.sys.path
+    # print os.sys.path
     
     # scapy_cap = rdpcap('file.pcap')
     # for packet in scapy_cap:
     #     print packet[IPv6].src
+    
+    aPacket = IP(ttl=10)
+    print(aPacket)
+    print(aPacket.src)
+    print(aPacket.dst)
+    
+    hexdump(aPacket)
+    b = raw(aPacket)
+    print(b)
+    c = Ether(b)
+    print(c)
 
 if __name__ == "__main__":
     main()
